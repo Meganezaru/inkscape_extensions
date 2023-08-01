f69ea4d30599799cbf8ca8a3b1fbfd16f219c0df
Kazuya Goto <kaag.fc@gmail.com>
2023-08-01 22:37:25 +0900

    mod:rewite arrange_circle with ChatGPT4

--- arrange_circle/arrange_circle.py
+++ arrange_circle/arrange_circle.py
@@ -1,46 +1,33 @@
-#!/usr/bin/env python
 import inkex
+import math
 
-class ArrangeCircles(inkex.Effect):
-    def __init__(self):
-        inkex.Effect.__init__(self)
-        self.arg_parser.add_argument("--rows", type=int, dest="rows", default=5, help="Number of rows")
-        self.arg_parser.add_argument("--columns", type=int, dest="columns", default=5, help="Number of columns")
-        self.arg_parser.add_argument("--circle_size", type=float, dest="circle_size", default=20.0, help="Size of the circles")
-        self.arg_parser.add_argument("--spacing", type=float, dest="spacing", default=5.0, help="Spacing between circles")
+class CreateCircles(inkex.GenerateExtension):
+    container_label = 'Create Circles'
+    rows_option = 'rows'
+    cols_option = 'cols'
+    diameter_option = 'diameter'
+    space_option = 'space'
 
-    def effect(self):
+    def add_arguments(self, pars):
+        pars.add_argument("--rows", type=int, help="Number of rows")
+        pars.add_argument("--cols", type=int, help="Number of columns")
+        pars.add_argument("--diameter", type=int, help="Diameter of circles")
+        pars.add_argument("--space", type=int, help="Space between circles")
+
+    def generate(self):
         rows = self.options.rows
-        columns = self.options.columns
-        circle_size = self.options.circle_size
-        spacing = self.options.spacing
+        cols = self.options.cols
+        diameter = self.options.diameter
+        space = self.options.space
 
-        # Calculate the offsets for each row to place circles at the vertices of equilateral triangles
-        x_offset = circle_size * 3**0.5 / 2
-        y_offset = circle_size * 1.5
+        offset = diameter + space
+        for r in range(rows):
+            for c in range(cols):
+                x = (c + 0.5 * (r % 2)) * offset
+                y = r * offset * math.sqrt(3) / 2
+                style = {'stroke': '#000000', 'fill': '#ffffff'}
+                attrs = {'style': str(inkex.Style(style)), 'cx': str(x), 'cy': str(y), 'r': str(diameter/2)}
+                yield inkex.Circle(**attrs)
 
-        # Create a new layer to place the circles
-        layer = inkex.etree.SubElement(self.get_svg().getroot(), 'g')
-        layer.set(inkex.addNS('label', 'inkscape'), 'Circles')
-
-        # Loop through rows and columns to place circles
-        for row in range(rows):
-            for col in range(columns):
-                # Calculate the center coordinates of the circle
-                center_x = col * (circle_size + spacing) + (row % 2) * x_offset
-                center_y = row * (circle_size + spacing) * 0.866 + spacing + y_offset
-
-                # Create the circle element
-                circle = inkex.etree.Element(inkex.addNS('circle', 'svg'))
-                circle.set('cx', str(center_x))
-                circle.set('cy', str(center_y))
-                circle.set('r', str(circle_size / 2))
-                circle.set('style', 'fill:none;stroke:black;stroke-width:1')
-
-                # Append the circle to the layer
-                layer.append(circle)
-
-# Create an instance of the ArrangeCircles class and apply the effect
-if __name__ == '__main__':
-    effect = ArrangeCircles()
-    effect.run()
+if __name__ == "__main__":
+    CreateCircles().run()
